4cc1c087bb816f6997c75f26acd2fd0d5c62a5c0
EcMer <eric.chraibi@mercantour-parcnational.fr>
2024-08-05 15:34:38 +0200

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,8 +17,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'myst_parser'
-]
+    'myst_parser']
 
 
 intersphinx_mapping = {
@@ -37,5 +36,5 @@ html_theme = 'sphinx_rtd_theme'
 epub_show_urls = 'footnote'
 
 
-]
+
 source_suffix = ['.rst', '.md']
